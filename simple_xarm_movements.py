8551e0a2902e6ac9882308c4ec538d896db8a246
sar-valliappan <173072045+sar-valliappan@users.noreply.github.com>
2026-04-24 14:31:31 -0700

    simple_xarm_movements: refactor with preset gripper widths and joint angles

--- simple_xarm_movements.py
+++ simple_xarm_movements.py
@@ -9,25 +9,29 @@ arm.set_mode(0)
 arm.set_state(0)
 arm.reset(wait=True)
 
-#move to pick the object
-arm.set_gripper_position(850, wait=True)
-arm.set_position(*[300, 0, 300, 180, 0, 0], wait=True)
-arm.set_position(*[300, 0, 200, 180, 0, 0], wait=True)
-arm.set_gripper_position(0, wait=True)
+# move to pick the object
+# move to pick the object
+arm.set_servo_angle(angle=[0, -30, 0, 0, 0, 0], wait=True)
+arm.set_servo_angle(angle=[-120, -30, 0, 0, 0, 0], wait=True)
+arm.set_gripper_position(350, wait=True)
+arm.set_servo_angle(angle=[-120, -18, -4, 0, 0, 0], wait=True)
+arm.set_gripper_position(280, wait=True)
 
-#set tcp payload
+# set tcp payload
 arm.set_tcp_load(0.3, [0, 0, 30])
 arm.set_state(0)
 
-#move to place the object
-arm.set_position(*[300, 0, 300, 180, 0, 0], wait=True)
-arm.set_position(*[300, -150, 300, 180, 0, 0], wait=True)
-arm.set_position(*[300, -150, 200, 180, 0, 0], wait=True)
+# move to place the object
+arm.set_servo_angle(angle=[-120, -30, -4, 0, 0, 0], wait=True)   # lift j3 before moving
+arm.set_servo_angle(angle=[-160, -30, -4, 0, 0, 0], wait=True)   # move j1 to place
+arm.set_servo_angle(angle=[-160, -18, -4, 0, 0, 0], wait=True)  # descend j3
 arm.set_gripper_position(850, wait=True)
 arm.set_tcp_load(0, [0, 0, 30])
 arm.set_state(0)
-arm.set_position(*[300, -150, 300, 180, 0, 0], wait=True)
-arm.set_gripper_position(0, wait=True)
+
+arm.set_servo_angle(angle=[-160, -40, -4, 0, 0, 0], wait=True)   # lift away
+#arm.set_position(*[300, -150, 300, 180, 0, 0], wait=True)
+#arm.set_gripper_position(0, wait=True)
 
 #go back to home
 arm.move_gohome(wait=True)
